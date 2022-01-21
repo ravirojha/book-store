390982a90082539397dbd085d155357bffb7fe43
Geek <geek@Geeks-MacBook-Pro.local>
2022-01-21 18:13:36 +0530

    Adding book

--- src/service/book-service.ts
+++ src/service/book-service.ts
@@ -4,6 +4,7 @@ import { Util } from "../util";
 
 export default class BookService {
     fetchBooks = async ({minPrice=0, maxPrice=1000000, minQty=0, page=1}) => {
+        if (minPrice === 0 && maxPrice === 0) throw new Error("No Books found");
         if (page >= 3) return [];
         const books = [];
         await Util.sleep(2);
